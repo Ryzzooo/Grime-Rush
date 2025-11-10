760103179bf2642e1858a65792519ddb038c7aa6
Ryzzooo <sisaderay.888@gmail.com>
2025-11-10 16:44:15 +0700

    Benerin jalan

--- Assets/Scripts/Player Scripts/PlayerMove.cs
+++ Assets/Scripts/Player Scripts/PlayerMove.cs
@@ -3,24 +3,19 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private Animator anim; // <-- BARU: Variabel untuk Animator
+    private Animator anim;
     private Vector2 movement;
     public float speed;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>(); // <-- BARU: Ambil komponen Animator saat game dimulai
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         Move();
-
-        // <-- BARU: Kirim informasi ke Animator -->
-        // movement.magnitude akan bernilai 0 jika tidak ada input, 
-        // dan 1 jika ada input (karena Anda menggunakan .Normalize())
-        // Kita kirim 'true' ke parameter "IsMoving" jika magnitude > 0
         anim.SetBool("IsMoving", movement.magnitude > 0);
     }
 
